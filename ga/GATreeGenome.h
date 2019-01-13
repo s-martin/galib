8c96c53272061342279e75858d68c4275f96b6da
s-martin <saimen54@hotmail.com>
2019-01-13 20:14:32 +0100

    formatted

--- ga/GATreeGenome.h
+++ ga/GATreeGenome.h
@@ -3,7 +3,7 @@
   tree.h
   mbwall 25feb95
   Copyright (c) 1995 Massachusetts Institute of Technology
-                     all rights reserved
+					 all rights reserved
 
  DESCRIPTION:
   This header defines the interface for the tree genome.
@@ -11,9 +11,8 @@
 #ifndef _ga_tree_h_
 #define _ga_tree_h_
 
-#include <GATree.h>
 #include <GAGenome.h>
-
+#include <GATree.h>
 
 /* ----------------------------------------------------------------------------
 TreeGenome
@@ -22,49 +21,76 @@ TreeGenome
 on the tree, so if you have an objective function that encourages size the tree
 will grow until you run out of memory.
 ---------------------------------------------------------------------------- */
-template <class T>
-class GATreeGenome : public GATree<T>, public GAGenome {
-public:
-  GADeclareIdentity();
+template <class T> class GATreeGenome : public GATree<T>, public GAGenome
+{
+  public:
+	GADeclareIdentity();
 
-  static int DestructiveMutator(GAGenome &, float);
-  static int SwapNodeMutator(GAGenome &, float);
-  static int SwapSubtreeMutator(GAGenome &, float);
-  static int OnePointCrossover(const GAGenome&, const GAGenome&, 
-			      GAGenome*, GAGenome*);
-  static float TopologyComparator(const GAGenome&, const GAGenome&);
-//  static float NodeComparator(const GAGenome&, const GAGenome&);
+	static int DestructiveMutator(GAGenome &, float);
+	static int SwapNodeMutator(GAGenome &, float);
+	static int SwapSubtreeMutator(GAGenome &, float);
+	static int OnePointCrossover(const GAGenome &, const GAGenome &, GAGenome *,
+								 GAGenome *);
+	static float TopologyComparator(const GAGenome &, const GAGenome &);
+	//  static float NodeComparator(const GAGenome&, const GAGenome&);
 
-public:
-  GATreeGenome(GAGenome::Evaluator f=NULL, void * u=NULL);
-  GATreeGenome(const GATreeGenome<T> &);
-  GATreeGenome<T> & operator=(const GAGenome & orig)
-    {copy(orig); return *this;}
-  virtual ~GATreeGenome();
-  virtual GAGenome *clone(GAGenome::CloneMethod flag=CONTENTS) const;
-  virtual void copy(const GAGenome &);
+  public:
+	GATreeGenome(GAGenome::Evaluator f = NULL, void *u = NULL);
+	GATreeGenome(const GATreeGenome<T> &);
+	GATreeGenome<T> &operator=(const GAGenome &orig)
+	{
+		copy(orig);
+		return *this;
+	}
+	virtual ~GATreeGenome();
+	virtual GAGenome *clone(GAGenome::CloneMethod flag = CONTENTS) const;
+	virtual void copy(const GAGenome &);
 
 #ifdef GALIB_USE_STREAMS
-  virtual int write (std::ostream &) const;
+	virtual int write(std::ostream &) const;
 #endif
 
-  virtual int equal(const GAGenome & c) const;
+	virtual int equal(const GAGenome &c) const;
 
-// Here we do inlined versions of the access members of the super class.  We
-// do our own here so that we can set/unset the _evaluated flag appropriately.
+	// Here we do inlined versions of the access members of the super class.  We
+	// do our own here so that we can set/unset the _evaluated flag
+	// appropriately.
 
-  int destroy() { _evaluated = false; return GATree<T>::destroy(); }
-  int swaptree(GATree<T> * t)
-    { _evaluated = false; return GATree<T>::swaptree(t); }
-  int swaptree(unsigned int i, unsigned int j)
-    { _evaluated = false; return GATree<T>::swaptree(i,j); }
-  int swap(unsigned int i, unsigned int j)
-    { _evaluated = false; return GATree<T>::swap(i,j); }
-  GATree<T> * remove() { _evaluated = false; return GATree<T>::remove(); }
-  int insert(GATree<T> * t, GATreeBASE::Location where=GATreeBASE::BELOW)
-    { _evaluated = false; return GATree<T>::insert(t, where); }
-  int insert(const T & t, GATreeBASE::Location where=GATreeBASE::BELOW)
-    { _evaluated = false; return GATree<T>::insert(t, where); }
+	int destroy()
+	{
+		_evaluated = false;
+		return GATree<T>::destroy();
+	}
+	int swaptree(GATree<T> *t)
+	{
+		_evaluated = false;
+		return GATree<T>::swaptree(t);
+	}
+	int swaptree(unsigned int i, unsigned int j)
+	{
+		_evaluated = false;
+		return GATree<T>::swaptree(i, j);
+	}
+	int swap(unsigned int i, unsigned int j)
+	{
+		_evaluated = false;
+		return GATree<T>::swap(i, j);
+	}
+	GATree<T> *remove()
+	{
+		_evaluated = false;
+		return GATree<T>::remove();
+	}
+	int insert(GATree<T> *t, GATreeBASE::Location where = GATreeBASE::BELOW)
+	{
+		_evaluated = false;
+		return GATree<T>::insert(t, where);
+	}
+	int insert(const T &t, GATreeBASE::Location where = GATreeBASE::BELOW)
+	{
+		_evaluated = false;
+		return GATree<T>::insert(t, where);
+	}
 };
 
 #endif
