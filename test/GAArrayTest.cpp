c57f679f8f08df225b887b8dfc92c931c4056cc8
s-martin <saimen54@hotmail.com>
2019-09-07 15:21:16 +0200

    added unit tests for GAArray

---
+++ test/GAArrayTest.cpp
@@ -0,0 +1,55 @@
+#include <boost/test/unit_test.hpp>
+
+#include <GAArray.h>
+#include <iostream>
+
+
+BOOST_AUTO_TEST_SUITE( UnitTest )
+
+BOOST_AUTO_TEST_CASE(GAArray_001)
+{
+	GAArray<int> gaarray(2);
+	BOOST_CHECK_EQUAL(gaarray.size(), 2);
+	
+	// TODO remove unsigned from operator
+	BOOST_CHECK_EQUAL(gaarray[0u], 0);
+	BOOST_CHECK_EQUAL(gaarray[1u], 0);
+
+	int i1 = 1;
+	int i2 = 2;
+	gaarray[0u] = i1;
+	gaarray[1u] = i2;
+	
+	BOOST_CHECK_EQUAL(gaarray[0u], 1);
+	BOOST_CHECK_EQUAL(gaarray[1u], 2);
+
+	auto gaarray2 = gaarray;
+	BOOST_CHECK_EQUAL(gaarray2.size(), 2);
+	BOOST_CHECK_EQUAL(gaarray2[0u], 1);
+	BOOST_CHECK_EQUAL(gaarray2[1u], 2);
+
+	//BOOST_CHECK(gaarray == gaarray2);
+
+	GAArray gaarray3(gaarray);
+	BOOST_CHECK_EQUAL(gaarray3.size(), 2);
+	BOOST_CHECK_EQUAL(gaarray3[0u], 1);
+	BOOST_CHECK_EQUAL(gaarray3[1u], 2);
+
+	int simplearray[2];
+	simplearray[0] = 3;
+	simplearray[1] = 4;
+	BOOST_CHECK_EQUAL(simplearray[0u], 3);
+	BOOST_CHECK_EQUAL(simplearray[1u], 4);
+
+	GAArray<int> gaarray4(2);
+	gaarray4 = simplearray;
+	BOOST_CHECK_EQUAL(gaarray4.size(), 2);
+	BOOST_CHECK_EQUAL(gaarray4[0u], 3);
+	BOOST_CHECK_EQUAL(gaarray4[1u], 4);
+
+	//BOOST_CHECK(gaarray != gaarray4);
+
+	// TODO add failures to improve implementation
+}
+
+BOOST_AUTO_TEST_SUITE_END()
